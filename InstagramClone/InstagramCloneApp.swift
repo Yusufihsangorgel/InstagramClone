a242b772525a4b9dbc85d26220cc0891a5418859
Yusufihsangorgel <87409084+Yusufihsangorgel@users.noreply.github.com>
2022-10-08 23:09:01 +0300

    Initial Commit

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Yusuf İhsan Görgel on 8.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
