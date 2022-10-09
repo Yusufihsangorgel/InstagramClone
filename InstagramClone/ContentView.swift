40a4416dcec7d5ec467fd1b5bfabddb99a10b70d
Yusufihsangorgel <87409084+Yusufihsangorgel@users.noreply.github.com>
2022-10-09 14:21:25 +0300

    Update ContentView.swift

--- InstagramClone/ContentView.swift
+++ InstagramClone/ContentView.swift
@@ -8,15 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var text: String = ""
     var body: some View {
         VStack(spacing: 0.0) {
             Header()
             
             Storys()
            
-           
+            Divider()
             
+            
+            Post(text: $text)
+            
+            
+   
             Spacer()
+            
         }
         
     
@@ -88,5 +95,99 @@ struct Storys: View {
             .padding(.horizontal,8)
         }
         .padding(.vertical,10)
+    }
+}
+
+struct PostHeader: View {
+    var body: some View {
+        HStack {
+            HStack{
+                Image("profile")
+                    .resizable()
+                    .frame(width: 30, height: 30)
+                    .cornerRadius(50)
+                
+                Text("Yusuf İhsan")
+                    .font(.caption)
+                    .fontWeight(.bold)
+            }
+            Spacer()
+            
+            Image("more")
+        }
+        .padding(.vertical,10)
+        .padding(.horizontal,10)
+    }
+}
+
+struct PostContent: View {
+    var image: String = "profile"
+    var body: some View {
+        VStack {
+            Image(image)
+                .resizable()
+                .frame(width: .infinity)
+                .aspectRatio(contentMode: .fit)
+            HStack{
+                HStack(spacing: 10){
+                    Image("heart")
+                    Image("comment")
+                    Image("share")
+                }
+                Spacer()
+                Image("bookmark")
+            }
+            .padding(.vertical,5)
+            .padding(.horizontal,12)
+        }
+    }
+}
+
+struct Post: View {
+    var text : Binding<String>
+    var image: String = "profile"
+    var description: String = "I miss travling to Japan"
+    
+    var body: some View {
+        VStack(alignment: .leading){
+            PostHeader()
+            
+            PostContent()
+            
+            Text("Liked by Meng To and others")
+                .font(.footnote)
+                .frame(width: .infinity)
+                .padding(.horizontal,12)
+            
+            Text(description)
+                .font(.footnote)
+                .frame(width: .infinity)
+                .padding(.horizontal,12)
+            
+            HStack{
+                HStack(spacing:7.0){
+                    Image(image)
+                        .resizable()
+                        .frame(width: 24, height: 24)
+                        .cornerRadius(50)
+                    
+                    TextField("Add comment..", text: self.text)
+                    
+                }
+                
+                
+                Spacer()
+                
+                HStack{
+                    Text("😍")
+                    Text("😂")
+                    Image(systemName: "plus.circle")
+                        .foregroundColor(.secondary)
+                }
+            }
+            .padding(.horizontal,12)
+            .padding(.vertical,9)
+            
+        }
     }
 }
